3a5d039e45e682fc6f239ab157761bb2872569ef
Andreas 'count' Kotes <count@flatline.de>
2016-02-11 01:09:34 +0100

    clean up a lot more

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 var child_process = require('child_process');
+var proxyquire    = require('proxyquire').noCallThru();
 var events        = require('events');
-var mockery       = require('mockery');
 var chai          = require('chai');
 var sinon         = require('sinon');
 var expect        = chai.expect;
@@ -10,99 +10,71 @@ chai.use(require('dirty-chai'));
 require('mocha-sinon');
 
 describe('lib/main', function() {
-    describe('#init()', function() {
-        before(function(){
-            mockery.enable({
-                warnOnReplace: false,
-                warnOnUnregistered: false,
-                useCleanCache: true
+    beforeEach(function(){
+            // stub internal modules
+            debugwinStub       = sinon.stub();
+            debugwinStub.init  = sinon.stub();
+            serialStub         = sinon.stub();
+            serialStub.init    = sinon.stub();
+            clockStub          = sinon.stub();
+            clockStub.init     = sinon.stub();
+            appStub            = sinon.stub();
+            appStub.init       = sinon.stub();
+            appStub.run        = sinon.stub();
+
+            // stub stub modules
+            consoleStub        = sinon.stub();
+            consoleStub.init   = sinon.stub();
+            processStub        = sinon.stub();
+            processStub.init   = sinon.stub();
+            navigatorStub      = sinon.stub();
+            navigatorStub.init = sinon.stub();
+
+            main = proxyquire('lib/main', {
+                'lib/debugwin':       debugwinStub,
+                'lib/serial':         serialStub,
+                'lib/clock':          clockStub,
+                'lib/app':            appStub,
+
+                'lib/console_stub':   consoleStub,
+                'lib/process_stub':   processStub,
+                //'lib/navigator_stub': navigatorStub,
             });
-        });
+    });
 
-        beforeEach(function(){
-            // stub all internal modules
-            debugwinStub = sinon.stub();
-            debugwinStub.init = sinon.stub();
-            mockery.registerMock('lib/debugwin', debugwinStub);
-            serialStub = sinon.stub();
-            serialStub.init = sinon.stub();
-            mockery.registerMock('lib/serial', serialStub);
-            clockStub = sinon.stub();
-            clockStub.init = sinon.stub();
-            mockery.registerMock('lib/clock', clockStub);
-            appStub = sinon.stub();
-            appStub.init = sinon.stub();
-            mockery.registerMock('lib/app', appStub);
-        });
-
-        after(function(){
-            mockery.disable();
-        });
-
-        it('should send an info line to console', function() {
-            var main = require('lib/main');
-            // let's grap the output
-            this.sinon.stub(console, 'info');
-
-            main.init();
-            expect( console.info.calledOnce ).to.be.true();
-            expect( console.info.calledWith("c-base-pos v0.8; using JQuery disabled; node " + process.versions.node + "; userAgent undefined") ).to.be.true();
-        });
-
-        it('should load and initialize several internal modules', function() {
-            var main = require('lib/main');
+    describe('#init()', function() {
+        it('should init several internal modules', function() {
             // shut up ;)
             this.sinon.stub(console, 'info');
 
             main.init();
             expect( debugwinStub.init.calledOnce ).to.be.true();
-            expect( serialStub.init.calledOnce ).to.be.true();
-            expect( clockStub.init.calledOnce ).to.be.true();
-            expect( appStub.init.calledOnce ).to.be.true();
+            expect( serialStub.init.calledOnce   ).to.be.true();
+            expect( clockStub.init.calledOnce    ).to.be.true();
+            expect( appStub.init.calledOnce      ).to.be.true();
+        });
+
+        it('should send an info line to console', function() {
+            // let's grab the output
+            this.sinon.stub(console, 'info');
+
+            main.init();
+            expect( console.info.calledOnce ).to.be.true();
+            expect( console.info.calledWith("c-base-pos v0.8; using JQuery disabled; node " + process.versions.node + "; userAgent none") ).to.be.true();
         });
     });
     describe('#run()', function() {
-        before(function(){
-            mockery.enable({
-                warnOnReplace: false,
-                warnOnUnregistered: false,
-                useCleanCache: true
-            });
-        });
-
-        beforeEach(function(){
-            // stub all internal modules
-            debugwinStub = sinon.stub();
-            debugwinStub.init = sinon.stub();
-            mockery.registerMock('lib/debugwin', debugwinStub);
-            serialStub = sinon.stub();
-            serialStub.init = sinon.stub();
-            mockery.registerMock('lib/serial', serialStub);
-            clockStub = sinon.stub();
-            clockStub.init = sinon.stub();
-            mockery.registerMock('lib/clock', clockStub);
-            appStub = sinon.stub();
-            appStub.init = sinon.stub();
-            appStub.run = sinon.stub();
-            mockery.registerMock('lib/app', appStub);
-        });
-
-        after(function(){
-            mockery.disable();
-        });
-
         it('should run the main app when run itself', function() {
-            var main = require('lib/main');
             // shut up ;)
             this.sinon.stub(console, 'info');
 
             main.init();
             main.run();
             expect( debugwinStub.init.calledOnce ).to.be.true();
-            expect( serialStub.init.calledOnce ).to.be.true();
-            expect( clockStub.init.calledOnce ).to.be.true();
-            expect( appStub.init.calledOnce ).to.be.true();
-            expect( appStub.run.calledOnce ).to.be.true();
+            expect( serialStub.init.calledOnce   ).to.be.true();
+            expect( clockStub.init.calledOnce    ).to.be.true();
+            expect( appStub.init.calledOnce      ).to.be.true();
+            expect( appStub.run.calledOnce       ).to.be.true();
         });
     });
 });
