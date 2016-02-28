3f44a2735e00de841c2b5c394b66b8e8c55fb54b
Andreas 'count' Kotes <count@flatline.de>
2016-02-28 20:19:52 +0100

    version display & serial & co

--- c-base-pos.js
+++ c-base-pos.js
@@ -3,7 +3,7 @@ requirejs.config({
     paths: {
       'sprintf-js': 'node_modules/sprintf-js/dist/sprintf.min',
       'jquery': 'node_modules/jquery/dist/jquery.min',
-      'serialport': 'node_modules/serialport'
+      'serialport': 'node_modules/serialport/serialport'
     }
 });
 
